354bbf0c95a837a9d2f1e350355ffaecafa11246
Leo Back <leo.back@outlook.de>
2025-04-07 15:57:38 +0200

    chore: apply prettier & eslint rules

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,27 @@
-import {View} from "react-native";
-import {Link} from "expo-router";
+import { View, StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-      <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-      >
-        <Link href="/camera">Camera</Link>
-        <Link href="/voice">Voice</Link>
-      </View>
+    <View style={styles.container}>
+      <Link href="/camera" style={styles.link}>
+        <Text>Camera</Text>
+      </Link>
+      <Link href="/voice" style={styles.link}>
+        <Text>Voice</Text>
+      </Link>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  link: {
+    padding: 10,
+    marginVertical: 5,
+  },
+});
