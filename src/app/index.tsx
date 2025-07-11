aeebc66f4d871e10a517f2b8f4298a5909b24ecc
Leo Back <leo.back@outlook.de>
2025-07-11 16:01:30 +0200

    feat: main layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,3 @@
-import { View, StyleSheet, Text } from "react-native";
-import { Link } from "expo-router";
+import TestIdentifier from "@/src/components/TestIdentifier/TestIdentifier";
 
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Link href="/root" style={styles.link}>
-        <Text>Camera</Text>
-      </Link>
-      <Link href="/voice" style={styles.link}>
-        <Text>Voice</Text>
-      </Link>
-      <Link href="/webview" style={styles.link}>
-        <Text>Webview</Text>
-      </Link>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  link: {
-    padding: 10,
-    marginVertical: 5,
-  },
-});
+export default TestIdentifier;
--- src/app/root/index.tsx
+++
@@ -1,5 +0,0 @@
-import TestIdentifier from "@/src/components/TestIdentifier/TestIdentifier";
-
-export default function Index() {
-  return <TestIdentifier />;
-}
